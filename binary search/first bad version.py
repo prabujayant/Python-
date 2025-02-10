a298c728766c06073f316af264fbffc3813ce0f7
Prabu Jayant <139766986+prabujayant@users.noreply.github.com>
2025-02-10 10:06:43 +0530

    Create first bad version.py

---
+++ binary search/first bad version.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        L = 1
+        R = n
+
+        while L < R:
+            M = (L+R) // 2
+            if isBadVersion(M):
+                R = M
+            else:
+                L = M + 1
+        
+        return L 
+        # Time: O(Log n)
+        # Space: O(1)
